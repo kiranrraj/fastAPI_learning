fce0e5909d2b4f41ffb7d03449413f6f1d3bdb5b
Kiran Raj R <kiranrajvjd@gmail.com>
2025-06-27 22:26:27 +0530

    P26 GBeeX-App -- Update 13
    Check point

--- P26_labx_app_v2/__gbeex-app/src/app/constants/tabs/tab.constants.ts
+++
@@ -1,7 +0,0 @@
-// src/app/constants/tabs/tab.constants.ts
-
-// Maximum number of tabs allowed to be open at once
-export const MAX_OPEN_TABS = 10;
-
-// Tooltip message when limit is hit
-export const MAX_TAB_TOOLTIP = "Maximum tab limit reached. Please close a tab to open a new one.";
--- P26_labx_app_v2/__gbeex-app/src/app/page.tsx
+++
@@ -1,4 +0,0 @@
-// src/app/page.tsx
-export default function HomePage() {
-  return <div className="p-4 text-lg font-semibold">Welcome to Gbeex App</div>;
-}
